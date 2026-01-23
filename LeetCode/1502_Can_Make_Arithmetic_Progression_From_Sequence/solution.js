b71c2eda55f0ca8393dc26de9e6980dc2d365a33
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-01-23 12:56:54 +0000

    Add solution for Can Make Arithmetic Progression From Sequence in javascript

---
+++ LeetCode/1502_Can_Make_Arithmetic_Progression_From_Sequence/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function(arr) {
+    arr = arr.sort((a,b)=>a-b);
+    var diff = arr[1]-arr[0]
+    for(let i=0;i<arr.length-1;i++){
+        if((arr[i+1]-arr[i])!=diff)
+            return false;
+    }
+    return true
+};
