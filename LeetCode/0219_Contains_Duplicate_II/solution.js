eca4521e2f086b720cf0e393a6179ef14aa8c481
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-02-03 11:22:33 +0000

    Add solution for Contains Duplicate II in javascript

---
+++ LeetCode/0219_Contains_Duplicate_II/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let obj={}
+    for(let i=0;i<nums.length;i++){
+        if(obj[nums[i]] == undefined) obj[nums[i]] =i;
+        else{
+            if(Math.abs(obj[nums[i]]-i)<=k) return true;
+            else obj[nums[i]] =i;
+        }
+    }
+        return false;
+};
