b38ff57d25443a68f5099dd18ac71eac766bd3b6
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-02-04 12:03:07 +0000

    Add solution for Reverse Words in a String in javascript

---
+++ LeetCode/0151_Reverse_Words_in_a_String/solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (str) {
+    let temp = ""
+    let s = ""
+    for (let i = 0; i < str.length; i++) {
+        if (i == 0 && str[i] == " ") continue;
+        if (str[i] == " " && str[i - 1] == " ") continue;
+        else if (str[i] != " ") temp += str[i];
+        else {
+            if(s!="")s = temp + " " + s;
+            else s = temp
+            temp = ""
+        }
+        if(i==str.length-1 && str[i]!=" ") {
+            if(s!="")s = temp + " " + s;
+            else s = temp
+        }
+    }
+
+
+    return s;
+};
