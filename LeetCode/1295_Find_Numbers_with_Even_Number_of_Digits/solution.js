e38334471643b43811d102c06d39a56dd937fa46
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-02-12 16:59:20 +0000

    Add solution for Find Numbers with Even Number of Digits in javascript

---
+++ LeetCode/1295_Find_Numbers_with_Even_Number_of_Digits/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let count=0;
+    for(let i=0;i<nums.length;i++){
+        let temp=String(nums[i]);
+        if(temp.length%2==0)count++
+    }
+    return count
+};
