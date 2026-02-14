c2444188d062839237ef05e24d096c650bc059f8
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-02-14 13:37:15 +0000

    Add solution for Sign of the Product of an Array in javascript

---
+++ LeetCode/1822_Sign_of_the_Product_of_an_Array/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+    let count=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==0)return 0;
+        else if (nums[i]<0) count++
+    }
+    if(count%2==0)return 1
+    else return -1
+};
