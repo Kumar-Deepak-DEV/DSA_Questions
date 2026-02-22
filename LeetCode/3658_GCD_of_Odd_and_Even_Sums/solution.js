c77ad6609aabf1edf4cc3d52e243bca5732139bd
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-02-22 16:31:45 +0000

    Add solution for GCD of Odd and Even Sums in javascript

---
+++ LeetCode/3658_GCD_of_Odd_and_Even_Sums/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var gcdOfOddEvenSums = function(n) {
+    // let so=0;
+    // let se=0;
+    // let o=1;
+    // let e=2;
+    // for(let i=0;i<n;i++){
+    //     so+=o;
+    //     se+=e;
+    //     o+=2;
+    //     e+=2;
+    // }
+    // for(let i=so;i>0;i--){
+    //     if(so%i==0 && se%i==0)return i;
+    // }
+
+    // let so=n*n
+    // let se=n*(n+1)
+
+    // for(let i=so;i>0;i--){
+    //     if(so%i==0 && se%i==0)return i;
+    // }
+
+    return n;
+};
