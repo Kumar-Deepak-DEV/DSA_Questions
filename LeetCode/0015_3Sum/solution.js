c5b9fb0468c394c95c6413d9c33609c97c43296c
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-03-26 16:20:10 +0000

    Add solution for 3Sum in javascript

---
+++ LeetCode/0015_3Sum/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+
+    nums = nums.sort((a, b) => a - b);
+    let res = []
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == nums[i - 1] && i > 0) continue;
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] + nums[i] == 0) {
+                res.push([nums[i], nums[left], nums[right]])
+                while (left < right && nums[left] === nums[left + 1]) left++
+                while (left < right && nums[right] === nums[right - 1]) right--
+                left++;
+                right--;
+            }
+            else if (nums[left] + nums[right] + nums[i] > 0) right--;
+            else left++;
+        }
+    }
+    return res;
+};
