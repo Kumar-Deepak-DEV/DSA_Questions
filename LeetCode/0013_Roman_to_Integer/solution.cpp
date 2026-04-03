a79cdd1e199bf1ae705494036fd24fc04263e1a8
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-04-03 16:40:38 +0000

    Add solution for Roman to Integer in cpp

---
+++ LeetCode/0013_Roman_to_Integer/solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int count  = 0;
+        for(int i =0; i< s.size();i++){
+            if(s[i] == 'M') count+=1000;
+            else if(s[i] == 'C' && s[i+1] == 'M') {
+                count +=900;
+                i++;
+            }
+            else if(s[i] == 'D') count +=500;
+            else if(s[i]=='C' && s[i+1] == 'D') {
+                count+=400;
+                i++;
+            }
+            else if(s[i] == 'C') count+=100;
+            else if(s[i] == 'X' && s[i+1] == 'C' ){
+                i++;
+                count+=90;
+            }
+            else if(s[i] == 'L') count+=50;
+            else if(s[i] == 'X' && s[i+1] == 'L') {
+                i++;
+                count+=40;
+            }
+            else if(s[i]=='X') count+=10;
+            else if(s[i] == 'I'  && s[i+1] == 'X' ){
+                i++;
+                count+=9;
+            } 
+            else if(s[i] == 'V') count+=5;
+            else if(s[i] == 'I' && s[i+1] == 'V') {
+                count+=4;
+                i++;
+            }
+            else if(s[i] == 'I') count++;
+
+        }
+        return count;
+    }
+};
