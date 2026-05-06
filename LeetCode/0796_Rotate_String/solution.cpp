0f99b03440497375d588031faaf3aef04839106e
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-05-06 17:08:14 +0000

    Add solution for Rotate String in cpp

---
+++ LeetCode/0796_Rotate_String/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length())return false;
+        string fin = s+s;
+        if(fin.find(goal)!=-1) return true;
+        return false;
+    }
+};
