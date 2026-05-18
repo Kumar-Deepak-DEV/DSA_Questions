bc595035b47efd54959efc0cbfe9ca37668c6d9a
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-05-18 14:54:53 +0000

    Add solution for Split a String in Balanced Strings in cpp

---
+++ LeetCode/1221_Split_a_String_in_Balanced_Strings/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int r = 0;
+        int l = 0;
+        int res = 0;
+        for(int i = 0; i<s.size();i++){
+            if(s[i] == 'R') r++;
+            if(s[i] == 'L') l++;
+            if(r==l) {
+                res++;
+                r=0;
+                l=0;
+            }
+        }
+        return res;
+    }
+};
