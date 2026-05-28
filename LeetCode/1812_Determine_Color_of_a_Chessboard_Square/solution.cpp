afe11ffa4d3c30d0100e94d5c990b53bac255968
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-05-28 08:58:38 +0000

    Add solution for Determine Color of a Chessboard Square in cpp

---
+++ LeetCode/1812_Determine_Color_of_a_Chessboard_Square/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        int a = coordinates[0] - 'a' + 1;
+        int b = coordinates[1] - '0';
+        if ((a % 2 == 0 && b % 2 == 1) || (a % 2 == 1 && b % 2 == 0))
+            return true;
+        else
+            return false;
+    }
+};
