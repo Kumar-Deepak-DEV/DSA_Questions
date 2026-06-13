1d0228a8492f3c8653b1aa80dfabf6e55f34a687
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-06-13 04:42:32 +0000

    Add solution for Find Words Containing Character in cpp

---
+++ LeetCode/2942_Find_Words_Containing_Character/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> arr;
+        for (int i = 0; i < words.size(); i++) {
+            if(words[i].find(x) !=-1)  arr.push_back(i);
+        }
+        return arr;
+    }
+};
