59e16a78aafe28c41d70428f51f7d88e92120ec0
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-06-15 03:46:51 +0000

    Add solution for Number of Segments in a String in cpp

---
+++ LeetCode/0434_Number_of_Segments_in_a_String/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        int count = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (i == 0 && s[i] != ' ')
+                count++;
+            else if (s[i] != ' ' && s[i - 1] == ' ')
+                count++;
+        }
+        return count;
+    }
+};
