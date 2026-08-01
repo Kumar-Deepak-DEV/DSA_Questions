1c107dcb1edb14506d12e750cbf617e6edd0481a
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-08-01 14:49:46 +0000

    Add solution for Count Valid Prefixes in cpp

---
+++ LeetCode/4006_Count_Valid_Prefixes/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int count0 = 0;
+        int count1 = 0;
+        int ans = 0;
+        for(char x : s){
+            if(x == '1')count1++;
+            else count0++;
+            if(abs(count0-count1) <=1) ans++;
+        }
+        return ans;
+    }
+};
